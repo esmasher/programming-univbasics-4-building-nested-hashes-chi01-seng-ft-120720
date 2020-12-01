7b80818d169bed9fd731f1f205bd6df59c897e34
Esma Sherwani <esmasherr@gmail.com>
2020-12-01 23:24:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -57,7 +57,7 @@ monopoly = {
       :pieces => 4, 
       :names =>{
         :reading_railroad => {
-          "mortgage_value" => "$100"
+          "mortgage_value" => 100
           }, 
         :pennsylvania_railroad => {
           "mortgage_value" => "$200"
